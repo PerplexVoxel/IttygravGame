a95619ccb33093d3a2b704122764b253e4004096
DigitDaemon <sccasg.thomasclandry@gmail.com>
2019-03-05 11:10:59 -0800

    Added John's scripts to assets and created a octogon to use as a base for the position changing script.

---
+++ IttygravGame_Unity/Assets/Scripts/TransitionCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        
+        Vector3 _Vector2 = new Vector3(20, 20);
+        _Vector2 = _Vector2 + gameObject.transform.position;
+        Debug.DrawRay(gameObject.transform.position, _Vector2, Color.red);
+
+    }
+    
+}
